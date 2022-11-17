f4bedc0be1bdbd57abc609ad4107d2b98fbc67d7
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-11-16 17:32:41 -0900

    write custom each and any methods

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,30 @@
 module Enumerable
   # Your code goes here
+
+  def my_any?
+    
+    i = 0
+    x = false
+
+    while i < self.count
+
+      item = self[i]
+      
+      if yield(item)
+        x = true
+        i = self.count
+      else
+        x = false
+      end
+
+      i += 1
+
+    end
+
+    return x
+
+  end
+
 end
 
 # You will first have to define my_each
@@ -8,4 +33,18 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    if block_given?
+      i = 0
+      while i < self.count
+        item = self[i]
+        yield(item)
+        i += 1
+      end
+    end
+    return self
+  end
+
+  # [1,2,3].my_each {|item| puts item + 2}
+
 end
